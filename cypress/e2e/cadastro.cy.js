c4b6bce5640168c7c2d6881d8b30c964b44765ff
Felipe Lima <felipebarrozo@hotmail.com>
2022-09-01 19:19:48 -0300

    Iniciando o projeto, adicionando as depedências

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,9 @@
+///<reference types ="cypress"/>
+
+context('Cadastro de usuário', () => {
+    it('Criação de novo usuário com sucesso', () => {
+        cy.visit('https://bugbank.netlify.app/')
+        
+    });
+    
+});
